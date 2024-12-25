680d4b789169e333c63630560e303879ae850c4b
Fuaad <fuaadmohamoud@gmail.com>
2024-12-25 06:59:57 -0600

    Set up unicafe project with feedback system

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
+import { useState } from "react"
 
-
-import {useState} from 'react'
-
-const Display = ({counter}) => (<div>{counter}</div>)
-
-
-const Button = ({onClick,text}) =>(<button onClick={onClick}>{text}</button>)
-
-
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne= () => setCounter(counter-1)
-  const setToZero = () => setCounter(0)
-  
-
-  
-
-
-
-
-  
-
-  console.log('rendering...', counter)
-
+const Button = ({onClick,name}) =>{
   return (
-    <div>
-    <Display counter = {counter}></Display>
-    <Button onClick = {increaseByOne} text ="plus"></Button>
-    <Button onClick = {decreaseByOne} text ="minus"></Button>
-    <Button onClick = {setToZero} text = "zero"></Button>
-    </div>
+    <button onClick= {()=>onClick(name)}>{name}</button>
   )
 }
+
+const Display = ({stringName,name}) =>{
+  return (
+    <div>{stringName} {name}</div>
+  )
+}
+
+
+
+const App = () =>{
+
+  const handleClick = (name) =>{
+    if (name === "good"){
+      setGood(good+1)
+     
+    }else if (name === "neutral"){
+      setNeutral(neutral+1)
+      
+    }else if (name === "bad"){
+      setBad(bad+1)
+    }
+
+  }
+
+
+
+
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+
+  return <div>
+    <h1>give feedback</h1>
+    <Button onClick= {handleClick} name = "good">good</Button>
+    <Button onClick= {handleClick} name = "neutral">neutral</Button>
+    <Button onClick= {handleClick} name = "bad">bad</Button>
+    <h1>statistics</h1>
+    <Display stringName="good"name ={good} ></Display>
+    <Display stringName="neutral"name ={neutral} ></Display>
+    <Display stringName="bad"name ={bad} ></Display>
+  </div>
+  
+
+}
+
+
+
 
 
 export default App
